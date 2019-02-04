071735721691d491ac7f7f5301e7507ff11e6862
edvin3i <edviniii@gmail.com>
2019-02-04 06:02:17 +0500

    models created

---
+++ devbase/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DevbaseConfig(AppConfig):
+    name = 'devbase'
--- ugabase/settings.py
+++ ugabase/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(xndkbz)j$d-f9&nz3_^t500&pb73q02n-24cj-$)ltz%jxq%*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'devbase',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Yekaterinburg'
 
 USE_I18N = True
 
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
